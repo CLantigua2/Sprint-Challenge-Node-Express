053eab4545843f0087fbb38c89083cedd17786ce
Carlos Lantigua <clantigua954@gmail.com>
2018-11-09 13:06:21 -0500

    completed CRUD for project router

--- api/REST/projectMod.js
+++ api/REST/projectMod.js
@@ -3,62 +3,64 @@ const projectDB = require('../../data/helpers/projectModel');
 
 const router = express.Router();
 
+// get all projects
 router.get('/', (req, res) => {
+	projectDB
+		.get()
+		.then((project) => {
+			res.status(200).json(project);
+		})
+		.catch((err) => res.status(500).json({ message: "Can't retrieve whatever this data is!!" }));
+}); //http://localhost:9000/api/project
+
+//get projects by id
+router.get('/:id', (req, res) => {
 	const { id } = req.params;
 	projectDB
 		.get(id)
-		.then((action) => {
-			res.status(200).json(action);
+		.then((project) => {
+			res.status(200).json(project);
 		})
 		.catch((err) => res.status(500).json({ message: "Can't retrieve whatever this data is!!" }));
-});
-
-router.get('/', (req, res) => {
-	actionDB
-		.get()
-		.then((action) => {
-			res.status(200).json(action);
-		})
-		.catch((err) => res.status(500).json({ message: "Can't retrieve whatever this data is!!" }));
-}); //http://localhost:9000/api/action
+}); //http://localhost:9000/api/project/1 <== takes id
 
 router.post('/', (req, res) => {
-	actionDB
+	projectDB
 		.insert(req.body)
-		.then((action) => {
-			res.status(201).json(action);
+		.then((project) => {
+			res.status(201).json(project);
 		})
 		.catch((err) => {
-			res.status(500).json({ message: 'error creating that action!!', err });
+			res.status(500).json({ message: 'error creating that project!!', err });
 		});
-}); //http://localhost:9000/api/action => takes in project_id/description/notes/completed(optional)
+}); //http://localhost:9000/api/project => takes in project_id/description/notes/completed(optional)
 
 router.put('/:id', (req, res) => {
 	const { id } = req.params;
 	const changes = req.body;
-	actionDB
+	projectDB
 		.update(id, changes)
-		.then((action) => {
-			action
-				? res.status(200).json({ message: 'Action updated like a boss' })
-				: res.status(404).json({ message: 'That action does not exist' });
+		.then((project) => {
+			project
+				? res.status(200).json({ message: 'project updated like a boss' })
+				: res.status(404).json({ message: 'That project does not exist' });
 		})
 		.catch((err) => {
-			res.status(500).json({ message: 'error updating that action!!', err });
+			res.status(500).json({ message: 'error updating that project!!', err });
 		});
-}); //http://localhost:9000/api/action/1 <==id ==> takes in project_id/description/notes/completed(optional)
+}); //http://localhost:9000/api/project/1 <==id ==> takes in project_id/description/notes/completed(optional)
 
 router.delete('/:id', (req, res) => {
 	const { id } = req.params;
-	actionDB
+	projectDB
 		.remove(id)
 		.then((count) => {
 			count
-				? res.status(200).json({ message: `Removed ${count} action from database` })
-				: res.status(404).json({ message: "That action doesn't exist" });
+				? res.status(200).json({ message: `Removed ${count} project from database` })
+				: res.status(404).json({ message: "That project doesn't exist" });
 		})
 		.catch((err) => {
-			res.status(500).json({ message: 'error deleting that action!!', err });
+			res.status(500).json({ message: 'error deleting that project!!', err });
 		});
-}); //http://localhost:9000/api/action/1 <== takes an id for the action you want to delete
+}); //http://localhost:9000/api/project/1 <== takes an id for the project you want to delete
 module.exports = router;
